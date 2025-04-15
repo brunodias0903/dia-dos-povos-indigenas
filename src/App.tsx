cdd7f30b3f08c5dcc63a062e76976360081c179b
Bruno Dias de Vasconcelos <bdd.vasconcelos@icloud.com>
2025-04-15 14:44:31 -0400

    fix: footer import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Cultura from "@/pages/Cultura";
 import Home from "@/pages/Home";
